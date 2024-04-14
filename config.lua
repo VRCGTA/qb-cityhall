0e60a3899d9196774c56a8963a4975e3b67c9561
azutake <azusa_takemae@dwango.co.jp>
2024-04-14 15:22:22 +0900

    fix by yuka

--- config.lua
+++ config.lua
@@ -3,12 +3,12 @@ Config = Config or {}
 Config.UseTarget = GetConvar('UseTarget', 'false') == 'true' -- Use qb-target interactions (don't change this, go to your server.cfg and add `setr UseTarget true` to use this and just that from true to false or the other way around)
 
 Config.AvailableJobs = {                                     -- Only used when not using qb-jobs.
-    ['trucker'] = { ['label'] = 'Trucker', ['isManaged'] = false },
-    ['taxi'] = { ['label'] = 'Taxi', ['isManaged'] = false },
+    -- ['trucker'] = { ['label'] = 'Trucker', ['isManaged'] = false },
+    -- ['taxi'] = { ['label'] = 'Taxi', ['isManaged'] = false },
     ['tow'] = { ['label'] = 'Tow Truck', ['isManaged'] = false },
     -- ['reporter'] = { ['label'] = 'News Reporter', ['isManaged'] = false },
-    ['garbage'] = { ['label'] = 'Garbage Collector', ['isManaged'] = false },
-    ['bus'] = { ['label'] = 'Bus Driver', ['isManaged'] = false },
+    -- ['garbage'] = { ['label'] = 'Garbage Collector', ['isManaged'] = false },
+    -- ['bus'] = { ['label'] = 'Bus Driver', ['isManaged'] = false },
     ['hotdog'] = { ['label'] = 'Hot Dog Stand', ['isManaged'] = false }
 }
 
@@ -43,22 +43,6 @@ Config.Cityhalls = {
 }
 
 Config.DrivingSchools = {
-    { -- Driving School 1
-        coords = vec3(240.3, -1379.89, 33.74),
-        showBlip = true,
-        blipData = {
-            sprite = 225,
-            display = 4,
-            scale = 0.65,
-            colour = 3,
-            title = 'Driving School'
-        },
-        instructors = {
-            'DJD56142',
-            'DXT09752',
-            'SRI85140',
-        }
-    },
 }
 
 Config.Peds = {
